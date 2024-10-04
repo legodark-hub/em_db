9054d5abbc7ba2fec542d2c7ccf74173bd94d2b7
aidarminyazev <legodark@mail.ru>
2024-10-04 19:46:54 +0500

    docstring added

--- parser/downloader.py
+++ parser/downloader.py
@@ -3,6 +3,14 @@ import os
 
 
 def download_file(file_url, file_name, download_dir="downloaded_files"):
+    """
+    Downloads a file from the given URL and saves it to the given directory.
+
+    Args:
+        file_url (str): The URL of the file to download.
+        file_name (str): The name of the file to save.
+        download_dir (str): The directory to save the file to. Defaults to "downloaded_files".
+    """
     try:
         response = request.urlopen(file_url)
         with open(os.path.join(download_dir, file_name), "wb") as file:
