5ebce6bea2cf5afccbb887159fb2da5b3a6b6398
aidarminyazev <legodark@mail.ru>
2024-10-19 00:16:32 +0500

    async db conn

--- parser/database.py
+++ parser/database.py
@@ -1,13 +1,28 @@
-from sqlalchemy import create_engine
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncConnection
 from models import Base
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL)
+async_engine = create_async_engine(DATABASE_URL)
+
+async_session = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
 
 
-def create_db():
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+async def create_db():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+        
+async def get_async_connection() -> AsyncGenerator[AsyncConnection, None]:
+    async with async_engine.begin() as conn:
+        yield conn
+
+@asynccontextmanager
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
