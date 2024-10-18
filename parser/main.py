fe77f4966ef83098c4275eff3a6a7a7e784f581d
aidarminyazev <legodark@mail.ru>
2024-10-19 01:04:11 +0500

    upd

--- parser/main.py
+++ parser/main.py
@@ -3,7 +3,7 @@ import os
 from downloader import download_file
 from database import create_db
 from xls_to_db import xls_to_db
-import timeit
+import time
 import asyncio
 
 BASE_URL = "https://spimex.com/upload/reports/oil_xls/oil_xls_"
@@ -12,6 +12,7 @@ DOWNLOAD_DIR = "parser/xls_files"
 
 today = datetime.today()
 date_range = today - timedelta(days=30)
+
 
 async def main():
     tasks = []
@@ -30,11 +31,15 @@ async def main():
         file_url = f"{BASE_URL}{date_str}162000.xls"
         file_name = f"oil_xls_{date_str}.xls"
         
-        task = await xls_to_db(await download_file(file_url, file_name, DOWNLOAD_DIR), current_date)
+        download_task = await download_file(file_url, file_name, DOWNLOAD_DIR)
+        task = asyncio.create_task(xls_to_db(download_task, current_date))
         tasks.append(task)
         
         current_date += timedelta(days=1)
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    print(timeit.timeit(asyncio.run(main()), number=1))
+    start_time = time.perf_counter()
+    asyncio.run(main())
+    end_time = time.perf_counter()
+    print(f"Время выполнения: {end_time - start_time} сек.")
